a55b332e9f7175a69d7b5308774b07892ac6f2f4
Ziteng Wang <z@wzt.me>
2024-03-11 20:00:40 -0700

    Add repo template

---
+++ pkgs/sketch/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  stdenv,
+  coreutils,
+  jdk,
+  flex,
+  bison,
+  makeWrapper,
+}:
+stdenv.mkDerivation rec {
+  name = "sketch-${version}";
+  version = "1.7.6";
+  src = fetchTarball {
+    url = "https://people.csail.mit.edu/asolar/sketch-1.7.6.tar.gz";
+    sha256 = "0jhmqz2phhhhfcyms95cak3l5fjsj9smjsdz1njsds85c45iba2w";
+  };
+
+  nativeBuildInputs = [flex bison makeWrapper];
+  buildInputs = [jdk];
+
+  configurePhase = "cd sketch-backend && chmod +x ./configure && ./configure";
+  installPhase = ''
+    mkdir -p $out
+    cp -r ../sketch-backend $out
+    cp -r ../sketch-frontend $out
+
+    makeWrapper $out/sketch-frontend/sketch $out/bin/sketch \
+      --argv0 "sketch" \
+      --chdir $out/sketch-frontend \
+      --add-flags "--fe-inc=$out/sketch-frontend/sketchlib" \
+      --set SKETCH_HOME $out/sketch-frontend/runtime \
+      --set PATH ${lib.makeBinPath [
+      jdk
+      coreutils
+    ]}
+  '';
+}
