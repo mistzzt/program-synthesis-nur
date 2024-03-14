02adb0ba6cf787efc6d2c1ffc47775d9eec9d5a4
Ziteng Wang <z@wzt.me>
2024-03-13 22:51:39 -0700

    sketch executable now explicitly uses java

--- pkgs/sketch/default.nix
+++ pkgs/sketch/default.nix
@@ -18,18 +18,18 @@ stdenv.mkDerivation rec {
   version = "1.7.6";
   srcs = [
     (fetchurl
-    {
-      url = "https://people.csail.mit.edu/asolar/${name}.tar.gz";
-      sha256 = "5cdac9ce841fd532215ff9ad8cb61a38cbdf6de0a635a669d0e46cdae72da707";
-    })
+      {
+        url = "https://people.csail.mit.edu/asolar/${name}.tar.gz";
+        sha256 = "5cdac9ce841fd532215ff9ad8cb61a38cbdf6de0a635a669d0e46cdae72da707";
+      })
     (fetchFromGitHub
-    {
-      name = "sketch-backend";
-      owner = "asolarlez";
-      repo = "sketch-backend";
-      rev = "727c3de3ae49c87a6df877122e322e9b0aa76bc7";
-      sha256 = "79k3RJPoyqxGX3oyYJgbyd8teCTnoDyt91Sd/6aPPGU=";
-    })
+      {
+        name = "sketch-backend";
+        owner = "asolarlez";
+        repo = "sketch-backend";
+        rev = "727c3de3ae49c87a6df877122e322e9b0aa76bc7";
+        sha256 = "79k3RJPoyqxGX3oyYJgbyd8teCTnoDyt91Sd/6aPPGU=";
+      })
   ];
   sourceRoot = "sketch-backend";
   patches = [./backend.patch];
@@ -51,10 +51,10 @@ stdenv.mkDerivation rec {
     cp -r ../sketch-backend $out
     cp -r ../sketch-frontend $out
 
-    makeWrapper $out/sketch-frontend/sketch $out/bin/sketch \
+    makeWrapper ${jdk}/bin/java $out/bin/sketch \
       --argv0 "sketch" \
       --chdir $out/sketch-frontend \
-      --add-flags "--fe-inc=$out/sketch-frontend/sketchlib" \
+      --add-flags "-cp $out/sketch-frontend/${name}-noarch.jar -ea sketch.compiler.main.seq.SequentialSketchMain --fe-inc=$out/sketch-frontend/sketchlib" \
       --set SKETCH_HOME $out/sketch-frontend/runtime
   '';
 
