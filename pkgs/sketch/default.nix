014474fe868517ac8258d3dcb649bf5359414b32
Ziteng Wang <z@wzt.me>
2024-03-13 22:28:10 -0700

    Fix sketch not built on aarch64

--- pkgs/sketch/default.nix
+++ pkgs/sketch/default.nix
@@ -1,10 +1,14 @@
 {
   fetchurl,
+  fetchFromGitHub,
   lib,
   makeWrapper,
   stdenv,
   bison,
   flex,
+  autoconf,
+  automake,
+  libtool,
   jdk,
   which,
   coreutils,
@@ -12,15 +16,36 @@
 stdenv.mkDerivation rec {
   name = "sketch-${version}";
   version = "1.7.6";
-  src = fetchurl {
-    url = "https://people.csail.mit.edu/asolar/sketch-1.7.6.tar.gz";
-    sha256 = "5cdac9ce841fd532215ff9ad8cb61a38cbdf6de0a635a669d0e46cdae72da707";
-  };
+  srcs = [
+    (fetchurl
+    {
+      url = "https://people.csail.mit.edu/asolar/${name}.tar.gz";
+      sha256 = "5cdac9ce841fd532215ff9ad8cb61a38cbdf6de0a635a669d0e46cdae72da707";
+    })
+    (fetchFromGitHub
+    {
+      name = "sketch-backend";
+      owner = "asolarlez";
+      repo = "sketch-backend";
+      rev = "727c3de3ae49c87a6df877122e322e9b0aa76bc7";
+      sha256 = "79k3RJPoyqxGX3oyYJgbyd8teCTnoDyt91Sd/6aPPGU=";
+    })
+  ];
+  sourceRoot = "sketch-backend";
+  patches = [./backend.patch];
 
-  nativeBuildInputs = [flex bison makeWrapper];
+  nativeBuildInputs = [flex bison makeWrapper automake autoconf libtool];
   buildInputs = [jdk which coreutils];
 
-  configurePhase = "cd sketch-backend && chmod +x ./configure && ./configure";
+  preConfigure = ''
+    cd ..
+    mkdir sketch
+    mv ${name}/sketch-frontend sketch
+    mv sketch-backend sketch
+    rm -rf ${name}
+    cd sketch/sketch-backend
+    ./autogen.sh
+  '';
   installPhase = ''
     mkdir -p $out
     cp -r ../sketch-backend $out
