c5be3dd991815938e75e78be2f63770f91e30ec0
Ziteng Wang <z@wzt.me>
2024-03-14 23:35:51 -0700

    Fix sketch not built on x86_64-darwin

--- default.nix
+++ default.nix
@@ -3,6 +3,13 @@
   modules = import ./modules;
   overlays = import ./overlays;
 
-  sketch = pkgs.callPackage ./pkgs/sketch {};
+  sketch = pkgs.callPackage ./pkgs/sketch {
+    stdenv = let
+      inherit (pkgs) stdenv overrideSDK;
+    in
+      if stdenv.isDarwin
+      then overrideSDK stdenv "11.0"
+      else stdenv;
+  };
   reduce-algebra = pkgs.callPackage ./pkgs/reduce-algebra {};
 }
